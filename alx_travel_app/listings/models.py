46b583aac522cc1663cd774ee9d81b7e5e55edb7
Hanan Mohamud <hananmohamudy@gmail.com>
2025-05-31 22:58:13 +0300

    initial commit

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+class Listing(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Booking(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bookings")
+    check_in = models.DateField()
+    check_out = models.DateField()
+    status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("confirmed", "Confirmed"), ("cancelled", "Cancelled")])
+
+class Review(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
